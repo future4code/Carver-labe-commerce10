c1925cf151bd4c15e426af04236e77cad8660351
MatosLarissa <larissamatos88@outlook.com>
2021-11-11 11:30:13 -0300

    Organizando array

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Produtos from "./components/produtos/Produtos";
 import Carrinho from "./components/Carrinho";
 import styled from "styled-components";
 import ProdutosCard from "./components/produtos/ProdutosCard";
+// import fundo1 from "./components/produtos/img/fundo1.png"
 
 const ContainerMain = styled.div`
   border: 1px solid red;
-  background-color: #ffffff;
+  /* background-color: #ffffff; */
+  /* background-color: #dcfefd; */
+  background-image: url('https://www.pixel4k.com/wp-content/uploads/2018/12/galaxy-4k_1546278891.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
   display: flex;
   justify-content: space-between;
   padding: 1vh;
@@ -19,7 +24,7 @@ const ContainerDivProduto = styled.div`
   width: 90%;
   height: 43.8vw;
   margin: 0;
-  border: 1px solid black;
+  /* border: 1px solid black; */
 `;
 
 const ContainerDiv = styled.div`
--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -2,8 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const BoxProdutos = styled.div`
-  border: 1px solid yellow;
-  background-color:#a979fc;
+  /* border: 1px solid yellow; */
+  /* background-color: #71b810; */
+  background-color: #ffffff;
+  opacity: 0.5;
+  border-radius:10px;
+  box-shadow: 1px 1px 5px #625834;
   height: 85vh;
 
 `
