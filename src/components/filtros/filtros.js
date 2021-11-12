ebd4f2d7a3aba3f9a35164bca5544c8cba9dfced
MatosLarissa <larissamatos88@outlook.com>
2021-11-12 19:34:33 -0300

    .....

--- src/components/filtros/filtros.js
+++ src/components/filtros/filtros.js
@@ -5,21 +5,24 @@ import ProdutosCard from '../produtosCard/produtosCard'
 
 export default class Filtros extends React.Component {
     render() {
-    return (
-        <ContainerFiltro>
-            <input
-                placeholder="Pesquisa"
-                value={this.props.buscar} 
-                onChange={this.props.updateBusca}
-            />
-            {this.props.buscar
-            .filter(buscar => {
-                 return buscar.nome.includes(this.props.buscar)
-            })
-            .map(buscar => {
-                return <ProdutosCard key={buscar.id} buscar={buscar}/>
-            })}
-        </ContainerFiltro>
-    )
-  }
+        return (
+            <ContainerFiltro>
+                <input
+                    placeholder="Pesquisa"
+                    value={this.props.buscar}
+                    onChange={this.props.updateBuscar}
+                />
+                <input
+                    placeholder="Valor Mínimo"
+                    value={this.props.precoMin}
+                    onChange={this.props.updatePrecoMin}
+                />
+                <input
+                    placeholder="Valor Máximo"
+                    value={this.props.precoMax}
+                    onChange={this.props.updatePrecoMax}
+                />
+            </ContainerFiltro>
+)
+}
 }
